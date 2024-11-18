31008c83fa25e6f5caf87f2eccb3c949dbf30de5
ebejara <enrique@bejaranomail.com>
2024-11-18 19:24:38 +0100

    Added CMD args
    Command line arguments

--- XMLP/src/XMLP.cpp
+++ XMLP/src/XMLP.cpp
@@ -13,10 +13,40 @@
 
 
 
-int main() 
+int main(int argc, char *argv[]) 
 {
-	std::cout << "XML file will be loaded!" << std::endl;   
     Myxmldoc xlang;
+    char* filename[100];
+  //std::string filename;
+
+    if (argc == 1){
+	    std::cout << "Please XML-file name (incl. path) " << std::endl;
+        std::cout << "If no file name is given, then default XML file will be loaded." << std::endl;
+        std::cout << "XMLP>"; 
+        std::cin >> *filename;  
+      //  std::cout << "Filename: " << filename << std::endl;
+    }
+    else{
+        std::cout<<"Inside else: " << *argv[3]<< std::endl;
+        for(int i = 1; i <= argc; i++){
+            if( **argv == '-' ){
+                switch (*argv[i+1]){
+         
+                case 'o':
+                    //*filename = argv[i];
+                    std::cout << *argv[i]<< std::endl;
+                break;
+                
+                case 'h':
+                   std::cout << "Helptext" << std::endl;
+                break;
+                }
+            }
+            ++(*argv);
+        }
+    }
+     std::cout << "Filename is  " << filename << std::endl;
+     xlang.load_file(*filename);
 	//std::cout << "Load file Error code: " << xlang.load_sts_code(); 
     //std::cout << "  Description: " << xlang.load_sts_descr() << std::endl;
     //std::cout << std::endl;
@@ -26,7 +56,7 @@ int main()
     //std::cout << "Number on Menu item IDs is : " << xlang.count_of_menuitemID()<< std::endl;
     //std::cout << "ID 152 is: "<< xlang.find_TextbyID(152) << std::endl;
    // std::cout << "Add node code: " << xlang.append_node()<< std::endl;
-      std::cout << "Adding Notification: " << xlang.append_notification("Hello World")   << std::endl;
+    //  std::cout << "Adding Notification: " << xlang.append_notification("Hello World")   << std::endl;
     //xlang.printall();
     return 0;
 }
--- XMLP/src/myxml.hpp
+++ XMLP/src/myxml.hpp
@@ -26,16 +26,26 @@ class Myxmldoc
   /*Load the relevant child from the XML-file*/
   pugi::xml_node mastertxts = doc.child("TextExchangeFormat").child("MasterMessages").child("MasterMessageAdvanced");     
      
-        
+ int load_file(char *filename){
+    pugi::xml_parse_result result = doc.load_file(filename);
+    std::cout << "Error code: " << load_sts_code(result) << std::endl;
+    std::cout << "Error description: " << load_sts_descr(result) << std::endl;
+    if (load_sts_code(result) == 0){
+      std::cout << "XML file loaded"<< std::endl;
+    }
+
+      std::cout << "Debug text: "<< filename <<std::endl;
+    return 0;
+ }  
       
  /* Returns the error code of the XML-loading process*/
-  int load_sts_code()
+  int load_sts_code(pugi::xml_parse_result result)
   {
     return result.status; 
   };
 
  /* Returns the error code description of the XML-loading process*/
-  const char* load_sts_descr()
+  const char* load_sts_descr(pugi::xml_parse_result result)
   {
     
     return result.description();
@@ -48,14 +58,13 @@ class Myxmldoc
   
     for(mastertxt.first_child(); mastertxt; mastertxt = mastertxt.next_sibling("MasterMessageAdvanced"))
     {
-      
       if (mastertxt.first_child().text().as_int() == first_menu_text)
       {
         //std::cout << "In-If: " << mastertxt.first_child().text().as_int() << std::endl;
         return mastertxt.previous_sibling().first_child().text().as_int();
       }
     }
-            return 0;
+    return 0;
   };
 
   /*Counts the number of menu item Ids.*/
@@ -65,8 +74,6 @@ class Myxmldoc
     pugi::xml_node mastertxt = mastertxts;
     for(mastertxt.first_child(); mastertxt; mastertxt = mastertxt.next_sibling("MasterMessageAdvanced"))
     {
-      //std::cout << "MSGID " << msgid.child_value() << std::endl;
-     // if (first_menu_text.compare(mastertxt.child("MSGID").child_value()) == 0)
       if (mastertxt.first_child().text().as_int() < first_menu_text)
       {
         ++ncount;
@@ -76,7 +83,7 @@ class Myxmldoc
         return ncount;
       }
     }
-    return -1;
+    return -1; // return error.
   };
 
   /*Counts the number of menu item Ids.*/
@@ -92,7 +99,7 @@ class Myxmldoc
       }
     }
     return mcount;
-  };
+  }
 
    /*Finds the last menu item ID*/
   int find_last_menuitemID()
@@ -100,9 +107,9 @@ class Myxmldoc
     pugi::xml_node mastertxt = mastertxts;
     /*go to last mastertextsibling and then to its child "MSGID"*/
     return mastertxt.parent().last_child().first_child().text().as_int();
-  };
+  }
 
-  /*Finds teh corresponding master text to teh specified ID*/
+  /*Finds the corresponding master text to the specified ID*/
   const char* find_TextbyID(int ID)
   {
       pugi::xml_node mastertxt = mastertxts;
@@ -114,29 +121,29 @@ class Myxmldoc
         return mastertxt.last_child().text().as_string();
         }
       }
-    return "ID not found";
+    return "ID not found"; // Id was not found.
 
   };
 
  /* Checks if ID already exists*/
-  int ID_exists(int ID)
+  bool ID_exists(int ID)
   {
       pugi::xml_node mastertxt = mastertxts;
       for(mastertxt.first_child(); mastertxt; mastertxt = mastertxt.next_sibling("MasterMessageAdvanced"))
       {
         if (mastertxt.first_child().text().as_int()== ID){
-           return 1; // ID exixts in loaded XMl file
+           return true; // ID exixts in loaded XMl file
         }
       }
-      return 0;
+      return false;
   }
 
  
   int append_notification(const char* txt)
   {
     int last_ID = find_last_notID(); // find the last ID for Notifications.
-    
-    std::string idstr = std::to_string(last_ID + 1); // The new ID is string format
+    char idstr[4];
+    itoa(last_ID+1,idstr,10); // The Notification ID in text format 
     pugi::xml_node MasterMessages = mastertxts.parent();// "MasterMessages"
     pugi::xml_node MastertextAdvanced = mastertxts.parent().first_child();//Set to first item of "MastertextAdvanced"
     rootnodeTmp = doc.child("TextExchangeFormat");
@@ -144,51 +151,34 @@ class Myxmldoc
     tempDoc.save_file("../file/tempdoc.xml");
     std::cout << "tempDoc is now copy of original XML-file"<< std::endl<<std::endl;//Printing NewNot
     //tempDoc.print(std::cout,"",pugi::format_indent);
-    //itoa(++last_ID,idstr,10);
     std::cout << "string ID is :" << idstr << std::endl;//Printing out master text
-    MasterMessages.remove_children();
-    // std::cout << "LastID before for is :" << last_ID << std::endl;//Printing out master text
+    MasterMessages.remove_children(); //Clear node. To be filled up again in for-loop
+    std::cout << "LastID before for is :" << idstr << std::endl;//Printing out master text
     // std::cout << "Number of Not IDs:" << count_of_NotID() << std::endl;//Printing out master text
     std::cout << std::endl << std::endl << std::endl;
     // std::cout << "Print out all MastertextAdvanced texts "<< std::endl<<std::endl;//Printing MastertextAdvanced
         
-      /*  //for(MastertextAdvanced = mastertxts; MastertextAdvanced; MastertextAdvanced = MastertextAdvanced.next_sibling("MasterMessageAdvanced"))
-        
+        for(MastertextAdvanced = mastertxts; MastertextAdvanced; MastertextAdvanced = MastertextAdvanced.next_sibling("MasterMessageAdvanced"))
         {
-      
-         
-          std::cout << "Current MSG ID: " << MastertextAdvanced.first_child().text().as_int() << std::endl;
-          std::cout << "Next MSG ID:" << MastertextAdvanced.next_sibling().first_child().text().as_int() << std::endl;
+         // std::cout << "Current MSG ID: " << MastertextAdvanced.first_child().text().as_int() << std::endl;
+          //std::cout << "Next MSG ID:" << MastertextAdvanced.next_sibling().first_child().text().as_int() << std::endl;
            MasterMessages.append_copy(MastertextAdvanced);
           // MastertextAdvanced.print(std::cout,"",pugi::format_indent);
          if (MastertextAdvanced.first_child().text().as_int() == last_ID)
           {
             std::cout << "Inside if" << std::endl;
             MasterMessages.append_copy(MastertextAdvanced);
-            MasterMessages.last_child().first_child().first_child().set_value("1083");
+            MasterMessages.last_child().first_child().first_child().set_value(idstr);
             MasterMessages.last_child().last_child().first_child().set_value(txt);
           }
             MastertextAdvanced = MastertextAdvanced.next_sibling("MasterMessageAdvanced");
           
-        }*/
-        while (MastertextAdvanced.next_sibling("MasterMessageAdvanced") != nullptr){
-            std::cout << "Current MSG ID: " << MastertextAdvanced.first_child().text().as_int() << std::endl;
-            std::cout << "Next MSG ID:" << MastertextAdvanced.next_sibling().first_child().text().as_int() << std::endl;
-             MasterMessages.append_copy(MastertextAdvanced);
-             // MastertextAdvanced.print(std::cout,"",pugi::format_indent);
-             if (MastertextAdvanced.first_child().text().as_int() == last_ID)
-              {
-               std::cout << "Inside if" << std::endl;
-               MasterMessages.append_copy(MastertextAdvanced);
-               MasterMessages.last_child().first_child().first_child().set_value("1083");
-               MasterMessages.last_child().last_child().first_child().set_value(txt);
-             }
-            MastertextAdvanced = MastertextAdvanced.next_sibling("MasterMessageAdvanced");
         }
+       
            
       std::cout << "printing Mastermessages"<< std::endl<< std::endl<< std::endl;
       MasterMessages.print(std::cout,"",pugi::format_indent);
-      return -2;          
+      return 0;          
   }
 
   void save_file()
